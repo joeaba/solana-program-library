196c52238fcfd30ea3e68babf652915b8b35e9a9
joeaba <77398477+joeaba@users.noreply.github.com>
2021-10-01 15:57:56 +0530

    adding the commend to print file

--- docs/publish-docs.sh
+++ docs/publish-docs.sh
@@ -8,7 +8,8 @@ fi
 
 CONFIG_FILE=vercel.json
 
-cat vercel.json
+# cat vercel.json
+echo vercel.json
 
 PROJECT_NAME=solana-program-library
 
@@ -23,6 +24,8 @@ cat > "$CONFIG_FILE" <<EOF
   "scope": "solana-labs"
 }
 EOF
+echo "$CONFIG_FILE" 
+
 [[ -n $VERCEL_TOKEN ]] || {
   echo "VERCEL_TOKEN is undefined.  Needed for Vercel authentication."
   exit 1
