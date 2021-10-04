88729acb469d8b525c84191e9bf486b83b6efa3b
joeaba <77398477+joeaba@users.noreply.github.com>
2021-10-04 19:06:42 +0530

    changing publish.sh

--- docs/publish-docs.sh
+++ docs/publish-docs.sh
@@ -7,7 +7,7 @@ if [[ -d .vercel ]]; then
 fi
 
 CONFIG_FILE=vercel.json
-PROJECT_NAME=solana-program-library
+PROJECT_NAME=spl-solana-com
 
 PRODUCTION=
 if [[ -n "$CI" ]]; then
@@ -17,7 +17,7 @@ fi
 cat > "$CONFIG_FILE" <<EOF
 {
   "name": "$PROJECT_NAME",
-  "scope": "joeaba"
+  "scope": "solana-labs"
 }
 EOF
 
